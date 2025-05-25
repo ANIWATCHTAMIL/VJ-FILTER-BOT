fafc5d1bf5ceed0d39145d9f507d4575b1569bf5
ANIWATCHTAMIL <djsjjsjjajajsdjdjdj@gmail.com>
2025-05-25 10:47:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ id_pattern = re.compile(r'^.\d+$')
 SESSION = environ.get('SESSION', 'Aniwatch')
 API_ID = int(environ.get('API_ID', '27068498'))
 API_HASH = environ.get('API_HASH', 'ebcf398a11320aa4776da819d1d919c0')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7671893217:AAEfJ5KLSabQqrDuG8gytqFa8DY-FjJJ_08")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7531764528:AAHXJhinGjsytCk64I_hBYVmhgCKuT_of6o")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
@@ -26,7 +26,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002571214477'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002078559102'))
 
 # This Is File Channel Where You Upload Your File Then Bot Automatically Save It In Database 
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002219124955').split()]  # For Multiple Id Use One Space Between Each.
@@ -52,7 +52,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002078559102')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # This Channel Is For /batch command file store.
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002571214477')).split()]  # For Multiple Id Use One Space Between Each.
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]  # For Multiple Id Use One Space Between Each.
 
 # This Channel Is For Delete Index File, Forward Your File In This Channel Which You Want To Delete Then Bot Automatically Delete That File From Database.
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]  # For Multiple Id Use One Space Between Each.
@@ -60,8 +60,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://djsjjsjjajajsdjdjdj:ZqBaYrXfzw8asIpy@cluster0.ftmjmcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "Aniwatchfilterbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'aniwatchfilters')
+DATABASE_NAME = environ.get('DATABASE_NAME', "aniwatchfilterbot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'aniwatchindiafilters')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
